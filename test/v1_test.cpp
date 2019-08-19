f7966f753ec4fb3fe62f10754b9f92f8b5dcc544
Leonhard Kipp <Leonhard.Kipp@web.de>
2019-08-19 22:14:09 +0200

    Added testcase

--- test/v1_test.cpp
+++ test/v1_test.cpp
@@ -1,8 +1,9 @@
 //
 // Created by leonhard on 19.08.19.
 //
-#include <iostream>
 #include "../catch.hpp"
+#include <iostream>
+#include <map>
 #include "../src/wam/bfs_organizer/bfs_organizer.h"
 
 using namespace std;
@@ -14,6 +15,9 @@ TEST_CASE("v1 tests") {
         vector<string> code;
         code.push_back("natSymb(o). %o ist ein natsymb");
         code.push_back("natSymb(s(A)) :- natSymb(A). %o ist ein natsymb");
+        code.push_back("test(X) :- test(X).");
+        code.push_back("test(a).");
+        code.push_back("test(b).");
 
     SECTION("a") {
         org.load_program(code);
@@ -21,6 +25,21 @@ TEST_CASE("v1 tests") {
         auto ans = org.get_answer();
         REQUIRE(ans.has_value());
         REQUIRE(ans->at(0).substitute == "o");
+    }
+    SECTION("c") {
+        org.load_program(code);
+        org.load_query("test(Z)");
+        map<string,bool> found;
+        auto ans = org.get_answer();
+        for(int i=0; i <5; i++){
+                REQUIRE(ans.has_value());
+                found[ans->at(0).substitute ] = true;
+                bool is_a_or_b = ans->at(0).substitute == "a" || ans->at(0).substitute == "b";
+                REQUIRE(is_a_or_b);
+                ans = org.get_answer();
+        }
+        REQUIRE(found["a"]);
+        REQUIRE(found["b"]);
     }
 
 }
