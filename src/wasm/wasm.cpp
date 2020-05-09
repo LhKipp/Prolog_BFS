5aa6678b48ef13270ef9775aa43075f557912a6a
Leonhard Kipp <Leonhard.Kipp@web.de>
2020-05-09 22:46:11 +0200

    Reimplemented BFSWrapper

--- src/wasm/wasm.cpp
+++ src/wasm/wasm.cpp
@@ -2,23 +2,64 @@
 #include <string>
 #include <sstream>
 #include <vector>
-#include "../wam/bfs_organizer/bfs_organizer.h"
-#include <wam/bfs_organizer/data/result.h>
-
+#include <wam/bfs_organizer/bfs_organizer.h>
 #include <emscripten/bind.h>
+
 using namespace emscripten;
 using namespace wam;
 
+
+class PrologBFSWasmWrapper{
+    wam::bfs_organizer bfs_organizer;
+public:
+
+    void clear(){
+        bfs_organizer.clear();
+    }
+
+    /**
+     * Checks whether the given code is valid prolog program code
+ the code to validate
+     */
+    compiler::error validateProgramCode(std::string code){
+        return bfs_organizer.validate_program(code);
+    }
+
+    /**
+     * Checks whether the given code is valid prolog query code
+ the code to validate
+     */
+    compiler::error validateQueryCode(std::string code){
+        return bfs_organizer.validate_query(code);
+    }
+
+    compiler::error loadProgram(std::string prog) {
+        return bfs_organizer.load_program(prog);
+    }
+
+    compiler::error loadQuery(std::string query) {
+        return bfs_organizer.load_query(query);
+    }
+
+    wam::result getAnswer() {
+        return bfs_organizer.get_answer();
+    }
+
+    wam::query_node getUnificationTree() const{
+        return bfs_organizer.get_unification_tree();
+    }
+};
+
 // Binding code
-EMSCRIPTEN_BINDINGS(bfs_organizer) {
-        class_<wam::bfs_organizer>("bfs_organizer")
+EMSCRIPTEN_BINDINGS(PrologBFSWasmWrapper) {
+        class_<PrologBFSWasmWrapper>("PrologBFSWasmWrapper")
                 .constructor()
-                .function("clear", &wam::bfs_organizer::clear)
-                .function("validateQueryCode", &wam::bfs_organizer::validate_query)
-                .function("validateProgramCode", &wam::bfs_organizer::validate_program)
-                .function("loadProgram", &wam::bfs_organizer::load_program)
-                .function("loadQuery", &wam::bfs_organizer::load_query)
-                .function("getAnswer", &wam::bfs_organizer::get_answer)
-                .function("getUnificationTree", &wam::bfs_organizer::get_unification_tree)
-        ;
+                .function("clear", &PrologBFSWasmWrapper::clear)
+                .function("validateQueryCode", &PrologBFSWasmWrapper::validateQueryCode)
+                .function("validateProgramCode", &PrologBFSWasmWrapper::validateProgramCode)
+                .function("loadProgram", &PrologBFSWasmWrapper::loadProgram)
+                .function("loadQuery", &PrologBFSWasmWrapper::loadQuery)
+                .function("getAnswer", &PrologBFSWasmWrapper::getAnswer)
+                .function("getUnificationTree", &PrologBFSWasmWrapper::getUnificationTree);
 }
+
