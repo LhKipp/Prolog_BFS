1cf404d22c8700a71a59ac4bcdf3f60a319e1f59
Leonhard Kipp <Leonhard.Kipp@web.de>
2020-05-10 12:31:24 +0200

    Fix equals predicate

--- src/wam/compiler/built_in_predicates/predicates/equals.cpp
+++ src/wam/compiler/built_in_predicates/predicates/equals.cpp
@@ -19,61 +19,73 @@ node wam::preds::equals_node_tree() {
     return pred;
 }
 
+/*
+ * This method assumes that if lhs_heap_i or rhs_heap_i is ref, it cant be further dereferenced
+ */
+bool wam::preds::heap_reg_equals(wam::executor& executor, regist lhs, regist rhs){
+    size_t lhs_heap_i;
+    size_t rhs_heap_i;
+    if(lhs.is_REF()){
+        lhs_heap_i = wam::deref(executor, lhs.index);
+        lhs = executor.heap_at(lhs_heap_i);
+    }
+    if(rhs.is_REF()){
+        rhs_heap_i = wam::deref(executor, rhs.index);
+        rhs = executor.heap_at(rhs_heap_i);
+    }
+    if(lhs.is_STR()){
+        lhs_heap_i = lhs.index;
+        lhs = executor.heap_at(lhs_heap_i);
+    }
+    if(rhs.is_STR()){
+        rhs_heap_i = rhs.index;
+        rhs = executor.heap_at(rhs_heap_i);
+    }
+
+    if(lhs.type != rhs.type){
+        return false;
+    }
+    if(lhs.is_REF()){
+        assert(rhs.is_REF());
+        //Always false because of check at top
+        //return lhs_heap_i == rhs_heap_i;
+        return lhs_heap_i == rhs_heap_i;
+    }
+    if(lhs.is_INT()){
+        assert(rhs.is_INT());
+        return lhs.get_int_val() == rhs.get_int_val();
+    }
+    if(lhs.is_FUN()){
+        assert(rhs.is_FUN());
+        const functor_view &functor1 = executor.functor_of(Storage_FUN_index {lhs.index});
+        const functor_view &functor2 = executor.functor_of(Storage_FUN_index {rhs.index});
+        if(functor1 != functor2){
+            return false;
+        }
+
+        for (int i = 0; i < functor1.arity; ++i) {
+            if(!heap_reg_equals(executor,
+                    executor.heap_at(lhs_heap_i + 1 + i),
+                    executor.heap_at(rhs_heap_i + 1 + i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+    //Didnt expect the type of lhs
+    assert(false);
+}
+
 void wam::preds::equals_check(wam::executor &executor, size_t lhs_x_reg_i, size_t rhs_x_reg_i) {
     //This is very similar to the unify algorithm
 #ifdef DEBUG
     std::cout << "equals_check" << std::endl;
 #endif
-    std::stack<size_t> PDL;
-    PDL.push(executor.registers[lhs_x_reg_i].index);
-    PDL.push(executor.registers[rhs_x_reg_i].index);
-
-    while (!(PDL.empty() || executor.failed())) {
-        size_t d1, d2;
-        if (executor.heap_at(PDL.top()).is_REF()){
-            d1 = deref(executor, executor.heap_at(PDL.top()));
-        } else {
-            d1 = PDL.top();
-        }
-        PDL.pop();
-        if (executor.heap_at(PDL.top()).is_REF()){
-            d2 = deref(executor, executor.heap_at(PDL.top()));
-        } else {
-            d2 = PDL.top();
-        }
-        PDL.pop();
-
-        if (executor.heap_at(d1).is_FUN()) {
-            --d1;
-        }
-        if (executor.heap_at(d2).is_FUN()) {
-            --d2;
-        }
-
-        if (d1 != d2) {
-            const regist &reg1 = executor.heap_at(d1);
-            const regist &reg2 = executor.heap_at(d2);
-
-            //If different registers and one of them is ref, its always fail
-            if (reg1.is_REF() || reg2.is_REF()) {
-                executor.set_failed();
-                return;
-            } else {
-                //If both registers are str (functors) we check that all inner terms are equal
-                //reg1 && reg2 are STR registers
-                const functor_view &functor1 = executor.functor_of(FUN_index{reg1.index});
-                const functor_view &functor2 = executor.functor_of(FUN_index{reg2.index});
-
-                if (functor1 == functor2) {
-                    for (int i = 1; i <= functor1.arity; ++i) {
-                        PDL.push(reg1.index + i);
-                        PDL.push(reg2.index + i);
-                    }
-                } else {
-                    executor.set_failed();
-                    return;
-                }
-            }
-        }
+    //Var equality means same var_string_name and same register in heap
+    //But both vars having same x_reg also means they are identical
+    regist lhs = executor.registers[lhs_x_reg_i];
+    regist rhs = executor.registers[rhs_x_reg_i];
+    if(!wam::preds::heap_reg_equals(executor, lhs, rhs)){
+        executor.set_failed();
     }
 }
--- test/built_in_preds/equals_pred.cpp
+++ test/built_in_preds/equals_pred.cpp
@@ -5,6 +5,13 @@
 using namespace wam;
 using namespace std;
 
+void make_eq_test_case(std::string q, bool ans_expected){
+    bfs_organizer org;
+    org.load_query(q);
+    auto ans1 = org.get_answer().get_answer();
+    REQUIRE(ans1.has_value() == ans_expected);
+    auto t1 = org.get_unification_tree();
+}
 TEST_CASE("Functor Equality") {
     bfs_organizer org;
 
@@ -12,6 +19,7 @@ TEST_CASE("Functor Equality") {
     org.load_query("eq(a, a).");
     auto ans = org.get_answer().get_answer();
     REQUIRE(ans.has_value());
+    org.get_unification_tree();
 }
 
 TEST_CASE("Functor Equality 2") {
@@ -24,7 +32,9 @@ TEST_CASE("Functor Equality 2") {
     org.load_query("eq(X, Y).");
     auto ans = org.get_answer().get_answer();
     REQUIRE(ans.has_value());
+    org.get_unification_tree();
 }
+
 
 TEST_CASE("Functor inequality") {
     bfs_organizer org;
@@ -36,6 +46,7 @@ TEST_CASE("Functor inequality") {
     org.load_query("eq(X, Y).");
     auto ans = org.get_answer().get_answer();
     REQUIRE(!ans.has_value());
+    org.get_unification_tree();
 }
 
 TEST_CASE("Functor inequality 2") {
@@ -48,6 +59,7 @@ TEST_CASE("Functor inequality 2") {
     org.load_query("eq(X, Y).");
     auto ans = org.get_answer().get_answer();
     REQUIRE(!ans.has_value());
+    org.get_unification_tree();
 }
 
 TEST_CASE("Functor inequality with var") {
@@ -60,6 +72,7 @@ TEST_CASE("Functor inequality with var") {
     org.load_query("eq(X, Y).");
     auto ans = org.get_answer().get_answer();
     REQUIRE(!ans.has_value());
+    org.get_unification_tree();
 }
 
 TEST_CASE("Var equality") {
@@ -67,6 +80,7 @@ TEST_CASE("Var equality") {
     org.load_query("X == X.");
     auto ans = org.get_answer().get_answer();
     REQUIRE(ans.has_value());
+    org.get_unification_tree();
 }
 
 TEST_CASE("Var inequality") {
@@ -74,6 +88,7 @@ TEST_CASE("Var inequality") {
     org.load_query("Y == X.");
     auto ans = org.get_answer().get_answer();
     REQUIRE(!ans.has_value());
+    org.get_unification_tree();
 }
 
 //TODO this code doenst compile under swipl because X is a singleton variable
@@ -88,12 +103,13 @@ TEST_CASE("Var inequality 2") {
     org.load_query("eq(A,B).");
     auto ans = org.get_answer().get_answer();
     REQUIRE(!ans.has_value());
+    org.get_unification_tree();
 }
 
 
 TEST_CASE("Var equality tree") {
     bfs_organizer org;
-    org.load_program("eq(X, X).");
+    org.load_program("eq(X, X) :- X == X.");
     org.load_query("eq(X, X).");
     auto ans1 = org.get_answer().get_answer();
     REQUIRE(ans1.has_value());
@@ -103,4 +119,20 @@ TEST_CASE("Var equality tree") {
     auto ans = org.get_answer().get_answer();
     auto t = org.get_unification_tree();
     REQUIRE(ans.has_value());
+}
+
+TEST_CASE("Int equality"){
+    make_eq_test_case("3 == 3.", true);
+}
+
+TEST_CASE("Int equality false"){
+    make_eq_test_case("3 ==4.", false);
+}
+
+TEST_CASE("Cons equality"){
+    make_eq_test_case("a == a.", true);
+}
+
+TEST_CASE("Cons inequality"){
+    make_eq_test_case("a == b.", false);
 }
