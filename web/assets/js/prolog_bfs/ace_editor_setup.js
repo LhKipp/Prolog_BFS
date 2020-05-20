791ee51a507a258fc4099f749855c2b440c95ea2
Martin Weber <enwi2@t-online.de>
2020-05-20 18:53:55 +0200

    Add help on first usage in frontend

--- web/assets/js/prolog_bfs/ace_editor_setup.js
+++ web/assets/js/prolog_bfs/ace_editor_setup.js
@@ -10,8 +10,10 @@ ace_program.setTheme("ace/theme/dreamweaver"); // textmate, dreamweaver
 ace_program.session.setMode("ace/mode/prolog");
 ace_program.setShowPrintMargin(false); // don't show the vertical line
 ace_program.commands.addCommand(ace_hotkey_run);
-if (Cookies.get("programcode")) { // only setValue when cookie exists
+if (Cookies.get("programcode")) { 
     ace_program.setValue(Cookies.get("programcode"));
+} else {
+    ace_program.setValue("your_program_code_here.");
 }
 
 var ace_query = ace.edit("query");
@@ -21,4 +23,6 @@ ace_query.setShowPrintMargin(false);
 ace_query.commands.addCommand(ace_hotkey_run);
 if (Cookies.get("querycode")) {
     ace_query.setValue(Cookies.get("querycode"));
+} else {
+    ace_query.setValue("your_query_code_here.");
 }
