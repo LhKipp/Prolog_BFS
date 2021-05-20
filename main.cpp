89e6278bfaf7ae13743ac2aafb349f478844f12a
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2021-05-21 01:43:11 +0200

    pass args to test

--- main.cpp
+++ main.cpp
@@ -11,20 +11,18 @@ int main(int argc, const char *argv[]) {
     using namespace std;
 
     const char*test_args[] = {""};
-    int result = Catch::Session().run(2, test_args);
+    int result = Catch::Session().run(argc, argv);
 
 
 //    Parsing CommandLine Arguments
-    program_arguments::parser parser{};
-    auto map = parser.getMap(argc, argv);
+    /* program_arguments::parser parser{}; */
+    /* auto map = parser.getMap(argc, argv); */
 
-    if (map.count(program_arguments::help_option)) {
-        cout << parser.generic << "\n";
-        return 0;
-    }
+    /* if (map.count(program_arguments::help_option)) { */
+    /*     cout << parser.generic << "\n"; */
+    /*     return 0; */
+    /* } */
 
     //return app_runner::normal_mode(map);
     return 0;
-
-
 }
