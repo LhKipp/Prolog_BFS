6012ba45bafa4317d17b6dd25c1697fc0352c03c
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2022-01-08 13:35:25 +0100

    remove unnecessary comments in cmakeLists.txt

--- build_native.sh
+++ build_native.sh
@@ -1,13 +1,2 @@
 #!/bin/sh
-# 
-# File:   build_native.sh
-# Author: martin
-#
-# Created on Aug 22, 2019, 4:39:14 PM
-#
-
-# the filename is hardcoded in cmake so we need to do
-# some ugly hacks here
-# cp CMakeLists.txt.native CMakeLists.txt
-
 cmake --build build -j 8 && cmake --install build
